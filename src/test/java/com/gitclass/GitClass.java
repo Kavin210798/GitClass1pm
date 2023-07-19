5fc854d5d0c462d96d8fca446274bce96e6fb93f
Kavin210798 <kavinponnuvel210798@gmail.com>
2023-07-19 16:10:50 +0530

    first time push

---
+++ src/test/java/com/gitclass/GitClass.java
@@ -0,0 +1,20 @@
+package com.gitclass;
+
+public class GitClass {
+	
+	
+	private void kavin() {
+		System.out.println("my name is kavin");
+		
+	}
+
+	private void sanjai() {
+		System.out.println("my brother name is sanjai");
+
+	}
+	
+	private void dharaneesh() {
+		System.out.println("my brother name is dharaneesh");
+
+	}
+}
